f7cd590ddfef5aba896f7dd41ad17d08b45a1fa3
johanvdp <johanvdp@users.noreply.github.com>
2021-01-11 03:01:14 +0100

    Move example info GUI

---
+++ main/Callback.h
@@ -0,0 +1,40 @@
+// https://stackoverflow.com/questions/19808054/convert-c-function-pointer-to-c-function-pointer/19809787
+// https://stackoverflow.com/users/6829808/evgeniy-alexeev
+// https://stackoverflow.com/help/licensing
+// CC BY-SA 3.0.
+#include <type_traits>
+#include <functional>
+
+template<typename T>
+struct ActualType {
+    typedef T type;
+};
+template<typename T>
+struct ActualType<T*> {
+    typedef typename ActualType<T>::type type;
+};
+
+template<typename T, unsigned int n,typename CallerType>
+struct Callback;
+
+template<typename Ret, typename ... Params, unsigned int n,typename CallerType>
+struct Callback<Ret(Params...), n,CallerType> {
+    typedef Ret (*ret_cb)(Params...);
+    template<typename ... Args>
+    static Ret callback(Args ... args) {
+        func(args...);
+    }
+
+    static ret_cb getCallback(std::function<Ret(Params...)> fn) {
+        func = fn;
+        return static_cast<ret_cb>(Callback<Ret(Params...), n,CallerType>::callback);
+    }
+
+    static std::function<Ret(Params...)> func;
+
+};
+
+template<typename Ret, typename ... Params, unsigned int n,typename CallerType>
+std::function<Ret(Params...)> Callback<Ret(Params...), n,CallerType>::func;
+
+#define GETCB(ptrtype,callertype) Callback<ActualType<ptrtype>::type,__COUNTER__,callertype>::getCallback
---
+++ main/main.cpp
@@ -0,0 +1,65 @@
+#include "freertos/FreeRTOS.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+
+#include "GUI.h"
+#include "LED.h"
+#include "AM2301.h"
+
+#define TAG "main"
+
+// GPIO configuration see Kconfig.projbuild
+#define LED_GPIO (gpio_num_t)CONFIG_LED_GPIO
+#define AM2301_GPIO (gpio_num_t)CONFIG_AM2301_GPIO
+
+extern "C" {
+
+GUI gui;
+LED led;
+AM2301 am2301;
+
+void app_main() {
+	ESP_LOGI(TAG, "app_main");
+
+	QueueHandle_t am2301ResultQueue = xQueueCreate(2, sizeof(AM2301::result_t));
+	if (am2301ResultQueue == 0) {
+		ESP_LOGE(TAG, "setup xQueueCreate failed (FATAL)");
+		return;
+	}
+
+	gui.setup();
+
+	led.setup(LED_GPIO, true);
+
+	am2301.setup(AM2301_GPIO, am2301ResultQueue);
+
+	AM2301::result_t am2301Result;
+	while (1) {
+
+		led.signal(1);
+		am2301.measure();
+		if (xQueueReceive(am2301ResultQueue, &am2301Result,
+				5000 / portTICK_PERIOD_MS)) {
+			if (am2301Result.status == AM2301::result_status_t::RESULT_OK) {
+				ESP_LOGI(TAG, "T:%.1fC RH:%.1f%%",
+						am2301Result.temperature - 273.15,
+						am2301Result.humidity);
+				led.signal(2);
+			} else if (am2301Result.status
+					== AM2301::result_status_t::RESULT_RECOVERABLE) {
+				ESP_LOGE(TAG, "app_main xQueueReceive RECOVERABLE");
+				// continue
+			} else if (am2301Result.status
+					== AM2301::result_status_t::RESULT_FATAL) {
+				ESP_LOGE(TAG, "app_main xQueueReceive FATAL");
+				// give up
+				break;
+			} else {
+				ESP_LOGE(TAG, "app_main status: %d", am2301Result.status);
+			}
+		}
+		vTaskDelay(5000 / portTICK_PERIOD_MS);
+	}
+}
+}
