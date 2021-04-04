bbde8365f9728ab79e02446d9270a33a45975dbc
johanvdp <johanvdp@users.noreply.github.com>
2021-04-04 15:41:30 +0200

    regroup model elements

--- main/model.c
+++ main/model.c
@@ -2,97 +2,125 @@
 
 #include "model.h"
 
+/******************
+ * actuator state
+ */
+
 const char *MODEL_ACTIVITY = "activity";
-
-const char *MODEL_EXHAUST = "exhaust.out";
-const char *MODEL_HEATER = "heater.out";
-const char *MODEL_LIGHT = "light.out";
-const char *MODEL_RECIRC = "recirc.out";
-
-const char *MODEL_AM2301_STATUS = "am2301.status";
-const char *MODEL_AM2301_TIMESTAMP = "am2301.timestamp";
-
-const char *MODEL_CIRCADIAN = "circadian";
-
-const char *MODEL_CO2_HI = "co2.hi";
-const char *MODEL_CO2_LO = "co2.lo";
-const char *MODEL_HUM_HI = "hum.hi";
-const char *MODEL_HUM_LO = "hum.lo";
-const char *MODEL_TEMP_HI = "temp.hi";
-const char *MODEL_TEMP_LO = "temp.lo";
-
-const char *MODEL_CONTROL_MODE = "control.mode";
-
-const char *MODEL_CO2_SV_DAY = "co2.sv.day";
-const char *MODEL_HUM_SV_DAY = "hum.sv.day";
-const char *MODEL_TEMP_SV_DAY = "temp.sv.day";
-
-const char *MODEL_CO2_PV = "co2.pv";
-const char *MODEL_HUM_PV = "hum.pv";
-const char *MODEL_TEMP_PV = "temp.pv";
-
-const char *MODEL_EXHAUST_SV = "exhaust.sv";
-const char *MODEL_HEATER_SV = "heater.sv";
-const char *MODEL_LIGHT_SV = "light.sv";
-const char *MODEL_RECIRC_SV = "recirc.sv";
-
-const char *MODEL_CO2_SV_NIGHT = "co2.sv.night";
-const char *MODEL_HUM_SV_NIGHT = "hum.sv.night";
-const char *MODEL_TEMP_SV_NIGHT = "temp.sv.night";
-
-const char *MODEL_CURRENT_TIME = "time";
-const char *MODEL_BEGIN_OF_DAY = "day";
-const char *MODEL_BEGIN_OF_NIGHT = "night";
-
-const char *MODEL_CO2_SV = "co2.sv";
-const char *MODEL_HUM_SV = "hum.sv";
-const char *MODEL_TEMP_SV = "temp.sv";
-
 pubsub_topic_t model_activity;
 
+const char *MODEL_EXHAUST = "exhaust.out";
 pubsub_topic_t model_exhaust;
+
+const char *MODEL_HEATER = "heater.out";
 pubsub_topic_t model_heater;
+
+const char *MODEL_LIGHT = "light.out";
 pubsub_topic_t model_light;
+
+const char *MODEL_RECIRC = "recirc.out";
 pubsub_topic_t model_recirc;
 
+/******************
+ * sensor state
+ */
+
+const char *MODEL_CURRENT_TIME = "time";
+pubsub_topic_t model_current_time;
+
+const char *MODEL_AM2301_STATUS = "am2301.status";
 pubsub_topic_t model_am2301_status;
+
+const char *MODEL_AM2301_TIMESTAMP = "am2301.time";
 pubsub_topic_t model_am2301_timestamp;
 
-pubsub_topic_t model_circadian;
-
-pubsub_topic_t model_co2_hi;
-pubsub_topic_t model_co2_lo;
-pubsub_topic_t model_hum_hi;
-pubsub_topic_t model_hum_lo;
-pubsub_topic_t model_temp_hi;
-pubsub_topic_t model_temp_lo;
-
-pubsub_topic_t model_control_mode;
-
-pubsub_topic_t model_co2_sv_day;
-pubsub_topic_t model_hum_sv_day;
-pubsub_topic_t model_temp_sv_day;
-
-pubsub_topic_t model_exhaust_sv;
-pubsub_topic_t model_heater_sv;
-pubsub_topic_t model_light_sv;
-pubsub_topic_t model_recirc_sv;
-
+const char *MODEL_CO2_PV = "co2.pv";
 pubsub_topic_t model_co2_pv;
+
+const char *MODEL_HUM_PV = "hum.pv";
 pubsub_topic_t model_hum_pv;
+
+const char *MODEL_TEMP_PV = "temp.pv";
 pubsub_topic_t model_temp_pv;
 
-pubsub_topic_t model_co2_sv_night;
-pubsub_topic_t model_hum_sv_night;
-pubsub_topic_t model_temp_sv_night;
+/******************
+ * controller state
+ */
 
-pubsub_topic_t model_current_time;
+const char *MODEL_CONTROL_MODE = "control.mode";
+pubsub_topic_t model_control_mode;
+
+const char *MODEL_CIRCADIAN = "circadian";
+pubsub_topic_t model_circadian;
+
+const char *MODEL_CO2_SV = "co2.sv";
+pubsub_topic_t model_co2_sv_day;
+
+const char *MODEL_HUM_SV = "hum.sv";
+pubsub_topic_t model_hum_sv_day;
+
+const char *MODEL_TEMP_SV = "temp.sv";
+pubsub_topic_t model_temp_sv_day;
+
+const char *MODEL_CO2_HI = "co2.hi";
+pubsub_topic_t model_co2_hi;
+
+const char *MODEL_CO2_LO = "co2.lo";
+pubsub_topic_t model_co2_lo;
+
+const char *MODEL_HUM_HI = "hum.hi";
+pubsub_topic_t model_hum_hi;
+
+const char *MODEL_HUM_LO = "hum.lo";
+pubsub_topic_t model_hum_lo;
+
+const char *MODEL_TEMP_HI = "temp.hi";
+pubsub_topic_t model_temp_hi;
+
+const char *MODEL_TEMP_LO = "temp.lo";
+pubsub_topic_t model_temp_lo;
+
+/******************
+ * setpoints
+ * (user settings)
+ */
+
+const char *MODEL_BEGIN_OF_DAY = "day";
 pubsub_topic_t model_begin_of_day;
+
+const char *MODEL_BEGIN_OF_NIGHT = "night";
 pubsub_topic_t model_begin_of_night;
 
+const char *MODEL_CO2_SV_DAY = "co2.sv.day";
 pubsub_topic_t model_co2_sv;
+
+const char *MODEL_CO2_SV_NIGHT = "co2.sv.night";
+pubsub_topic_t model_co2_sv_night;
+
+const char *MODEL_HUM_SV_DAY = "hum.sv.day";
 pubsub_topic_t model_hum_sv;
+
+const char *MODEL_HUM_SV_NIGHT = "hum.sv.night";
+pubsub_topic_t model_hum_sv_night;
+
+const char *MODEL_TEMP_SV_DAY = "temp.sv.day";
 pubsub_topic_t model_temp_sv;
+
+const char *MODEL_TEMP_SV_NIGHT = "temp.sv.night";
+pubsub_topic_t model_temp_sv_night;
+
+const char *MODEL_EXHAUST_SV = "exhaust.sv";
+pubsub_topic_t model_exhaust_sv;
+
+const char *MODEL_HEATER_SV = "heater.sv";
+pubsub_topic_t model_heater_sv;
+
+const char *MODEL_LIGHT_SV = "light.sv";
+pubsub_topic_t model_light_sv;
+
+const char *MODEL_RECIRC_SV = "recirc.sv";
+pubsub_topic_t model_recirc_sv;
+
 
 void model_initialize()
 {
--- main/model.h
+++ main/model.h
@@ -9,74 +9,125 @@ extern "C" {
 
 #include "pubsub.h"
 
+
+/******************
+ * actuator state
+ */
+
 /** Activity indicator (integer) */
 extern const char *MODEL_ACTIVITY;
+extern pubsub_topic_t model_activity;
 
 /** Exhaust fan actuator (boolean) */
 extern const char *MODEL_EXHAUST;
+extern pubsub_topic_t model_exhaust;
+
 /** Heater actuator (boolean) */
 extern const char *MODEL_HEATER;
+extern pubsub_topic_t model_heater;
+
 /** Light actuator (boolean) */
 extern const char *MODEL_LIGHT;
+extern pubsub_topic_t model_light;
+
 /** Recirculation fan actuator (boolean) */
 extern const char *MODEL_RECIRC;
+extern pubsub_topic_t model_recirc;
 
-/** AM2301 status (integer) */
+/******************
+ * sensor state
+ */
+
+/** Current time in seconds after epoch (integer, time_t) */
+extern const char *MODEL_CURRENT_TIME;
+extern pubsub_topic_t model_current_time;
+
+/** AM2301 status (integer, model_component_status_t) */
 extern const char *MODEL_AM2301_STATUS;
+extern pubsub_topic_t model_am2301_status;
+
 /** AM2301 measurement timestamp (integer) */
 extern const char *MODEL_AM2301_TIMESTAMP;
-
-/** Circadian (integer) */
-extern const char *MODEL_CIRCADIAN;
-
-/** Automatic control CO2 concentration high (boolean) */
-extern const char *MODEL_CO2_HI;
-/** Automatic control CO2 concentration low (boolean) */
-extern const char *MODEL_CO2_LO;
-/** Automatic control humidity high (boolean) */
-extern const char *MODEL_HUM_HI;
-/** Automatic control humidity low (boolean) */
-extern const char *MODEL_HUM_LO;
-/** Automatic control temperature high (boolean) */
-extern const char *MODEL_TEMP_HI;
-/** Automatic control temperature low (boolean) */
-extern const char *MODEL_TEMP_LO;
-
-/** Control mode (integer) */
-extern const char *MODEL_CONTROL_MODE;
-
-/** Day time CO2 concentration setpoint [ppm] (double) */
-extern const char *MODEL_CO2_SV_DAY;
-/** Day time humidity setpoint [%] (double) */
-extern const char *MODEL_HUM_SV_DAY;
-/** Day time temperature setpoint [K] (double) */
-extern const char *MODEL_TEMP_SV_DAY;
-
-/* Manual control exhaust fan setpoint (boolean) */
-extern const char *MODEL_EXHAUST_SV;
-/* Manual control heater setpoint (boolean) */
-extern const char *MODEL_HEATER_SV;
-/* Manual control light setpoint (boolean) */
-extern const char *MODEL_LIGHT_SV;
-/* Manual control recirculation fan setpoint (boolean) */
-extern const char *MODEL_RECIRC_SV;
+extern pubsub_topic_t model_am2301_timestamp;
 
 /** Measured CO2 concentration [ppm] (double) */
 extern const char *MODEL_CO2_PV;
+extern pubsub_topic_t model_co2_pv;
+
 /** Measured humidity [%] (double) */
 extern const char *MODEL_HUM_PV;
+extern pubsub_topic_t model_hum_pv;
+
 /** Measured temperature [K] (double) */
 extern const char *MODEL_TEMP_PV;
+extern pubsub_topic_t model_temp_pv;
 
-/** Night time CO2 concentration setpoint [ppm] (double) */
-extern const char *MODEL_CO2_SV_NIGHT;
-/** Night time humidity setpoint [%] (double) */
-extern const char *MODEL_HUM_SV_NIGHT;
-/** Night time temperature setpoint [K] (double) */
-extern const char *MODEL_TEMP_SV_NIGHT;
 
-/** Current time in seconds after epoch (time_t) */
-extern const char *MODEL_CURRENT_TIME;
+/******************
+ * controller state
+ */
+
+/** Control mode (integer, model_control_mode_t) */
+extern const char *MODEL_CONTROL_MODE;
+/** Control mode (integer, model_control_mode_t) */
+extern pubsub_topic_t model_control_mode;
+
+/** Circadian (integer, model_circadian_t) */
+extern const char *MODEL_CIRCADIAN;
+/** Circadian (integer, model_circadian_t) */
+extern pubsub_topic_t model_circadian;
+
+/** Current CO2 concentration setpoint [ppm] (double) */
+extern const char *MODEL_CO2_SV;
+/** Current CO2 concentration setpoint [ppm] (double) */
+extern pubsub_topic_t model_co2_sv;
+
+/** Current humidity setpoint [%] (double) */
+extern const char *MODEL_HUM_SV;
+/** Current humidity setpoint [%] (double) */
+extern pubsub_topic_t model_hum_sv;
+
+/** Current temperature setpoint [K] (double) */
+extern const char *MODEL_TEMP_SV;
+/** Current temperature setpoint [K] (double) */
+extern pubsub_topic_t model_temp_sv;
+
+/** Automatic control CO2 concentration high (boolean) */
+extern const char *MODEL_CO2_HI;
+/** Automatic control CO2 concentration high (boolean) */
+extern pubsub_topic_t model_co2_hi;
+
+/** Automatic control CO2 concentration low (boolean) */
+extern const char *MODEL_CO2_LO;
+/** Automatic control CO2 concentration low (boolean) */
+extern pubsub_topic_t model_co2_lo;
+
+/** Automatic control humidity high (boolean) */
+extern const char *MODEL_HUM_HI;
+/** Automatic control humidity high (boolean) */
+extern pubsub_topic_t model_hum_hi;
+
+/** Automatic control humidity low (boolean) */
+extern const char *MODEL_HUM_LO;
+/** Automatic control humidity low (boolean) */
+extern pubsub_topic_t model_hum_lo;
+
+/** Automatic control temperature high (boolean) */
+extern const char *MODEL_TEMP_HI;
+/** Automatic control temperature high (boolean) */
+extern pubsub_topic_t model_temp_hi;
+
+/** Automatic control temperature low (boolean) */
+extern const char *MODEL_TEMP_LO;
+/** Automatic control temperature low (boolean) */
+extern pubsub_topic_t model_temp_lo;
+
+
+/******************
+ * setpoints
+ * (user settings)
+ */
+
 /**
  * Begin of day in seconds after epoch (time_t)
  * always in the same day (2000-01-01)
@@ -86,77 +137,82 @@ extern const char *MODEL_BEGIN_OF_DAY;
  * Begin of day in seconds after epoch (time_t)
  * always in the same day (2000-01-01)
  */
-extern const char *MODEL_BEGIN_OF_NIGHT;
-
-/** Current CO2 concentration setpoint [ppm] (double) */
-extern const char *MODEL_CO2_SV;
-/** Current humidity setpoint [%] (double) */
-extern const char *MODEL_HUM_SV;
-/** Current temperature setpoint [K] (double) */
-extern const char *MODEL_TEMP_SV;
-
-extern pubsub_topic_t model_activity;
-
-extern pubsub_topic_t model_exhaust;
-extern pubsub_topic_t model_heater;
-extern pubsub_topic_t model_light;
-extern pubsub_topic_t model_recirc;
-
-extern pubsub_topic_t model_am2301_status;
-extern pubsub_topic_t model_am2301_timestamp;
-
-extern pubsub_topic_t model_circadian;
-
-/** Automatic control CO2 concentration high (boolean) */
-extern pubsub_topic_t model_co2_hi;
-/** Automatic control CO2 concentration low (boolean) */
-extern pubsub_topic_t model_co2_lo;
-/** Automatic control humidity high (boolean) */
-extern pubsub_topic_t model_hum_hi;
-/** Automatic control humidity low (boolean) */
-extern pubsub_topic_t model_hum_lo;
-/** Automatic control temperature high (boolean) */
-extern pubsub_topic_t model_temp_hi;
-/** Automatic control temperature low (boolean) */
-extern pubsub_topic_t model_temp_lo;
-
-extern pubsub_topic_t model_control_mode;
-
-extern pubsub_topic_t model_co2_sv_day;
-extern pubsub_topic_t model_hum_sv_day;
-extern pubsub_topic_t model_temp_sv_day;
-
-extern pubsub_topic_t model_exhaust_sv;
-extern pubsub_topic_t model_heater_sv;
-extern pubsub_topic_t model_light_sv;
-extern pubsub_topic_t model_recirc_sv;
-
-extern pubsub_topic_t model_co2_pv;
-extern pubsub_topic_t model_hum_pv;
-extern pubsub_topic_t model_temp_pv;
-
-extern pubsub_topic_t model_co2_sv_night;
-extern pubsub_topic_t model_hum_sv_night;
-extern pubsub_topic_t model_temp_sv_night;
-
-extern pubsub_topic_t model_current_time;
 extern pubsub_topic_t model_begin_of_day;
+
+/**
+ * Begin of day in seconds after epoch (time_t)
+ * always in the same day (2000-01-01)
+ */
+extern const char *MODEL_BEGIN_OF_NIGHT;
+/**
+ * Begin of day in seconds after epoch (time_t)
+ * always in the same day (2000-01-01)
+ */
 extern pubsub_topic_t model_begin_of_night;
 
-extern pubsub_topic_t model_co2_sv;
-extern pubsub_topic_t model_hum_sv;
-extern pubsub_topic_t model_temp_sv;
+/** Day time CO2 concentration setpoint [ppm] (double) */
+extern const char *MODEL_CO2_SV_DAY;
+/** Day time CO2 concentration setpoint [ppm] (double) */
+extern pubsub_topic_t model_co2_sv_day;
 
+/** Night time CO2 concentration setpoint [ppm] (double) */
+extern const char *MODEL_CO2_SV_NIGHT;
+/** Night time CO2 concentration setpoint [ppm] (double) */
+extern pubsub_topic_t model_co2_sv_night;
+
+/** Day time humidity setpoint [%] (double) */
+extern const char *MODEL_HUM_SV_DAY;
+/** Day time humidity setpoint [%] (double) */
+extern pubsub_topic_t model_hum_sv_day;
+
+/** Night time humidity setpoint [%] (double) */
+extern const char *MODEL_HUM_SV_NIGHT;
+/** Night time humidity setpoint [%] (double) */
+extern pubsub_topic_t model_hum_sv_night;
+
+/** Day time temperature setpoint [K] (double) */
+extern const char *MODEL_TEMP_SV_DAY;
+/** Day time temperature setpoint [K] (double) */
+extern pubsub_topic_t model_temp_sv_day;
+
+/** Night time temperature setpoint [K] (double) */
+extern const char *MODEL_TEMP_SV_NIGHT;
+/** Night time temperature setpoint [K] (double) */
+extern pubsub_topic_t model_temp_sv_night;
+
+/** Manual control exhaust fan setpoint (boolean) */
+extern const char *MODEL_EXHAUST_SV;
+/** Manual control exhaust fan setpoint (boolean) */
+extern pubsub_topic_t model_exhaust_sv;
+
+/** Manual control heater setpoint (boolean) */
+extern const char *MODEL_HEATER_SV;
+/** Manual control heater setpoint (boolean) */
+extern pubsub_topic_t model_heater_sv;
+
+/** Manual control light setpoint (boolean) */
+extern const char *MODEL_LIGHT_SV;
+/** Manual control light setpoint (boolean) */
+extern pubsub_topic_t model_light_sv;
+
+/** Manual control recirculation fan setpoint (boolean) */
+extern const char *MODEL_RECIRC_SV;
+/** Manual control recirculation fan setpoint (boolean) */
+extern pubsub_topic_t model_recirc_sv;
+
+/** Control mode */
 typedef enum
 {
     MODEL_CONTROL_MODE_OFF = 0, MODEL_CONTROL_MODE_MANUAL = 1, MODEL_CONTROL_MODE_AUTO = 2
 } model_control_mode_t;
 
+/** Component status */
 typedef enum
 {
     MODEL_COMPONENT_STATUS_OK = 0, MODEL_COMPONENT_STATUS_RECOVERABLE = 1, MODEL_COMPONENT_STATUS_FATAL = 2
 } model_component_status_t;
 
+/** Circadian phase */
 typedef enum
 {
     MODEL_CIRCADIAN_NIGHT = 0, MODEL_CIRCADIAN_DAY = 1
